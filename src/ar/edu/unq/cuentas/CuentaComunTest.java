f9dd66c0f60ae14ed487c0cae60d11adc66f9099
Eduardo61Martinez <mm7735608@gmail.com>
2025-04-05 22:36:03 -0300

    primerComit de cuenta 2

---
+++ src/ar/edu/unq/cuentas/CuentaComunTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package ar.edu.unq.cuentas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author marti
+ *
+ */
+class CuentaComunTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#getSaldo()}.
+	 */
+	@Test
+	void testGetSaldo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#setSaldo(double)}.
+	 */
+	@Test
+	void testSetSaldo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#getMaxNegativ()}.
+	 */
+	@Test
+	void testGetMaxNegativ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#setMaxNegativ(double)}.
+	 */
+	@Test
+	void testSetMaxNegativ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#saldoActual()}.
+	 */
+	@Test
+	void testSaldoActual() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ar.edu.unq.cuentas.CuentaComun#CuentaComun(double, double)}.
+	 */
+	@Test
+	void testCuentaComun() {
+		fail("Not yet implemented");
+	}
+
+}
